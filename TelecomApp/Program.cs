0dc213e8392999ea7d8de9a6ba0688ebf8d5ff4b
upron81 <upron81@users.noreply.github.com>
2024-10-09 18:33:23 +0300

    init project

---
+++ TelecomApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelecomApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
