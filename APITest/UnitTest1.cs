6f7c9cbe0283d133fe6da9ab0125331c928a3650
upron81 <upron81@users.noreply.github.com>
2024-10-31 15:41:35 +0300

    fix tests

--- APITest/UnitTest1.cs
+++ APITest/UnitTest1.cs
@@ -1,94 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using TelecomWeb.Controllers.tables;
 using TelecomWeb.Models;
 using Xunit;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Xunit.Abstractions;
-using TelecomWeb;
-using Microsoft.EntityFrameworkCore;
 
-namespace RestAPITests
+namespace TelecomWeb.Tests.Controllers
 {
-    public class SubscribersControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class SubscribersControllerTests : IDisposable
     {
-        private readonly HttpClient _client;
-        private readonly WebApplicationFactory<Program> _factory;
-        private readonly ITestOutputHelper _output;
-        private const int _subscriberId = 1;
+        private readonly TelecomDbContext _context;
+        private readonly SubscribersController _controller;
 
-        public SubscribersControllerTests(WebApplicationFactory<Program> factory, ITestOutputHelper output)
+        public SubscribersControllerTests()
         {
-            _factory = factory;
-            _client = _factory.CreateClient();
-            _output = output;
+            var options = new DbContextOptionsBuilder<TelecomDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _context = new TelecomDbContext(options);
+            _controller = new SubscribersController(_context);
         }
 
         [Fact]
-        public async Task Index_ReturnsViewResult_WithSubscribers()
+        public async Task Index_ReturnsViewResult_WithPaginatedList()
         {
-            var response = await _client.GetAsync("/Subscribers");
-            response.EnsureSuccessStatusCode();
-        }
-
-        [Fact]
-        public async Task Details_ReturnsNotFound_WhenSubscriberIdIsNull()
-        {
-            var response = await _client.GetAsync("/Subscribers/Details?Id=");
-            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [Fact]
-        public async Task Details_ReturnsViewResult_WithSubscriber_WhenIdIsValid()
-        {
-            var response = await _client.GetAsync($"/Subscribers/Details/{_subscriberId}");
-            response.EnsureSuccessStatusCode();
-        }
-
-        [Fact]
-        public async Task Edit_ReturnsNotFound_WhenSubscriberIdDoesNotExist()
-        {
-            var nonExistentId = 999999;
-            var response = await _client.GetAsync($"/Subscribers/Edit/{nonExistentId}");
-            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [Fact]
-        public async Task Edit_ReturnsViewResult_WhenSubscriberIsValid()
-        {
-            var updatedSubscriber = new FormUrlEncodedContent(new[]
+            var subscribers = new List<Subscriber>
             {
-                new KeyValuePair<string, string>("SubscriberId", _subscriberId.ToString()),
-                new KeyValuePair<string, string>("FullName", "John Doe Updated"),
-                new KeyValuePair<string, string>("HomeAddress", "456 Main St"),
-                new KeyValuePair<string, string>("PassportData", "987654321")
-            });
+                new Subscriber { SubscriberId = 1, FullName = "John Doe", HomeAddress = "123 Street" },
+                new Subscriber { SubscriberId = 2, FullName = "Jane Smith", HomeAddress = "456 Avenue" }
+            };
+            await _context.Subscribers.AddRangeAsync(subscribers);
+            await _context.SaveChangesAsync();
 
-            var response = await _client.PostAsync($"/Subscribers/Edit/{_subscriberId}", updatedSubscriber);
-            response.EnsureSuccessStatusCode();
-            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var result = await _controller.Index(null, null, null, null, 1) as ViewResult;
+
+            Assert.IsType<ViewResult>(result);
+            Assert.IsType<PaginatedList<Subscriber>>(result.Model);
+            Assert.Equal(2, ((PaginatedList<Subscriber>)result.Model).Count);
         }
 
         [Fact]
-        public async Task Delete_ReturnsNotFound_WhenSubscriberIdIsNull()
+        public async Task Details_ReturnsNotFound_WhenIdIsNull()
         {
-            var response = await _client.GetAsync("/Subscribers/Delete?Id=");
-            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var result = await _controller.Details(null, null, null);
+
+            Assert.IsType<NotFoundResult>(result);
         }
 
         [Fact]
-        public async Task Delete_ReturnsViewResult_WithSubscriber_WhenIdIsValid()
+        public async Task Details_ReturnsViewResult_WithSubscriber()
         {
-            var response = await _client.GetAsync($"/Subscribers/Delete/{_subscriberId}");
-            response.EnsureSuccessStatusCode();
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe", HomeAddress = "123 Street" };
+            await _context.Subscribers.AddAsync(subscriber);
+            await _context.SaveChangesAsync();
+
+            var result = await _controller.Details(1, null, null) as ViewResult;
+
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(subscriber, result.Model);
         }
 
         [Fact]
-        public async Task Statistic_ReturnsViewResult_WithSubscriberCount()
+        public async Task Create_RedirectsToIndex_OnValidModel()
         {
-            var response = await _client.GetAsync("/Subscribers/Statistic");
-            response.EnsureSuccessStatusCode();
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe" };
+
+            var result = await _controller.Create(subscriber) as RedirectToActionResult;
+
+            Assert.Equal("Index", result.ActionName);
+            Assert.Contains(subscriber, _context.Subscribers);
+        }
+
+        [Fact]
+        public async Task Create_ReturnsView_OnInvalidModel()
+        {
+            _controller.ModelState.AddModelError("Name", "Required");
+            var subscriber = new Subscriber();
+
+            var result = await _controller.Create(subscriber) as ViewResult;
+
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(subscriber, result.Model);
+        }
+
+        [Fact]
+        public async Task Edit_ReturnsNotFound_WhenIdsDoNotMatch()
+        {
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe" };
+
+            var result = await _controller.Edit(2, subscriber);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Edit_RedirectsToIndex_OnValidModel()
+        {
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe" };
+            await _context.Subscribers.AddAsync(subscriber);
+            await _context.SaveChangesAsync();
+
+            subscriber.FullName = "Updated Name";
+            var result = await _controller.Edit(1, subscriber) as RedirectToActionResult;
+
+            Assert.Equal("Index", result.ActionName);
+            Assert.Equal("Updated Name", _context.Subscribers.First().FullName);
+        }
+
+        [Fact]
+        public async Task DeleteConfirmed_RedirectsToIndex_OnSuccess()
+        {
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe" };
+            await _context.Subscribers.AddAsync(subscriber);
+            await _context.SaveChangesAsync();
+
+            var result = await _controller.DeleteConfirmed(1) as RedirectToActionResult;
+
+            Assert.Equal("Index", result.ActionName);
+            Assert.DoesNotContain(subscriber, _context.Subscribers);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsNotFound_WhenIdIsNull()
+        {
+            var result = await _controller.Delete(null);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsViewResult_WithSubscriber()
+        {
+            var subscriber = new Subscriber { SubscriberId = 1, FullName = "John Doe" };
+            await _context.Subscribers.AddAsync(subscriber);
+            await _context.SaveChangesAsync();
+
+            var result = await _controller.Delete(1) as ViewResult;
+
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(subscriber, result.Model);
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
         }
     }
 }
